e4cdeb1ac82299a36570ba4ff85565a1a0d09ed2
Andrej <andrejvaculciak@windowslive.com>
2017-11-12 23:26:42 +0100

    Add init_setup script for soekris

---
+++ docs/soekris-scripts/init_setup.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# The primary network interface
+printf '\n# The OpenFlow Switch interfaces\nauto enp6s0\niface enp6s0 inet manual\n\tpre-up ifconfig $IFACE promisc up\n\tpost-down ifconfig $IFACE promisc down\n\nauto enp10s0\niface enp10s0 inet manual\n\tpre-up ifconfig $IFACE promisc up\n\tpost-down ifconfig $IFACE promisc down\n\nauto enp11s0\niface enp11s0 inet manual\n\tpre-up ifconfig $IFACE promisc up\n\tpost-down ifconfig $IFACE promisc down' >> /etc/network/interfaces
+
+# Installation of Open vSwitch
+apt-get install openvswitch-switch -y
+
+# Creation of OF switch and add port to brigde
+service openvswitch-switch start
+ovs-vsctl add-br of-switch
+ovs-vsctl add-port of-switch enp6s0
+ovs-vsctl add-port of-switch enp10s0
+ovs-vsctl add-port of-switch enp11s0
+
+# Set OF vesion used by switch to 1.0 and 1.3
+ovs-vsctl set bridge of-switch protocols=OpenFlow10,OpenFlow13
+ovs-ofctl -O OpenFlow10,OpenFlow13 dump-flows of-switch
