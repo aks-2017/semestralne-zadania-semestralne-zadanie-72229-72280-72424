889faab9d42b1e5019fe8f31bac94e3a7010a98e
LukyGregor <laky.luky@gmail.com>
2017-11-24 23:15:49 +0100

    -change routes on flows -add stats of transfering bytes on ports

--- mininet-scripts/dtd_app.py
+++ mininet-scripts/dtd_app.py
@@ -1,119 +1,119 @@
 import requests
 import json
-import configparser
 import time
+import os
 
-upperThreshold = 0.5
-lowerThreshold = 0.3
+upperThreshold = 0.9
+lowerThreshold = 0.7
 primaryRoute = 0
 backupRoute = 1
 max_speed = 10485760
-
 state = primaryRoute
+B_to_MB = float(1024**2)
+
+controller = 'http://127.0.0.1:8080'
+h1_mac = '00:00:00:00:00:01'
+h2_mac = '00:00:00:00:00:02'
+h3_mac = '00:00:00:00:00:03'
+h4_mac = '00:00:00:00:00:04'
 
 class Router(object):
 	def __init__(self,j):
 		self.__dict__ = j.json()[0]
 
 def getPortStatsInfo():
-	r = requests.get(cntrllr_url + '/stats/port/1/3')
-	print(r.status_code)
-	h = r.json()['1'][0]['tx_bytes']
-	print('D: ' + str(h))
-	return h	
+	r = requests.get(controller + '/stats/port/1/4')
+	return r.json()['1'][0]['tx_bytes']
 
-def switchRoute(route):
-	url_sw1 = cntrllr_url + '/router/0000000000000001'
-	url_sw3 = cntrllr_url + '/router/0000000000000003' 
-	add_dst_sw1 = config['SW1-route']['dst1']
-	add_dst_sw3 = config['SW3-route']['dst1']
-	print('D1')
-	if route is primaryRoute:
-	   	gateway = config['SW1-route']['gateway1']
-	   	payload = {'destination': add_dst_sw1, 'gateway': gateway}
-	   	r = requests.post(url_sw1, data=json.dumps(payload))
-		
-		if r.status_code is 200:
-			print("Changed route to primary on SW1.")
-		
-		gateway = config['SW3-route']['gateway1']
-                payload = {'destination': add_dst_sw3, 'gateway': gateway}
-                r = requests.post(url, data=json.dumps(payload))
+def getPortStatsInfo1():
+        r = requests.get(controller + '/stats/port/2/2')
+        return r.json()['2'][0]['tx_bytes']	
 
-                if r.status_code is 200:
-                        print("Changed route to primary on SW3.")		
-
-	if route is backupRoute:
-		gateway = config['SW1-route']['bckpgateway']
-                payload = {'destination': add_dst_sw1, 'gateway': gateway}
-                r = requests.post(url, data=json.dumps(payload)) 
-		
-		if r.status_code is 200:
-                        print("Changed route to backup on SW1.")
+def add_flow(dpid, priority, src_mac, in_port, output_port):
+	url = controller + '/stats/flowentry/add'
 	
-		gateway = config['SW3-route']['bckpgateway']
-                payload = {'destination': add_dst_sw3, 'gateway': gateway}
-                r = requests.post(url, data=json.dumps(payload))
-
-                if r.status_code is 200:
-                        print("Changed route to backup on SW3.")
-
+	data = {}
+	data['dpid'] = dpid
+	data['priority'] = priority
+	data['match'] = {'eth_src': src_mac, 'in_port': in_port}
+	data['actions'] = [{'type': 'OUTPUT', 'port': output_port}]
 	
-def sw_conf(sw_id):
-	sw = "SW" + sw_id + "-add"
-	url = cntrllr_url + '/router/000000000000000' + sw_id
-
-	for ip in config[sw]['address'].split(','):
-		payload = {'address': ip}
-		r = requests.post(url, data=json.dumps(payload))
-
-	sw = "SW" + sw_id + "-route"
-
-	payload = {}
+	r = requests.post(url, json.dumps(data))
 	
-	for item in config[sw]:
-		if 'dst' in item:
-			payload['destination'] = config[sw][item]
-
-		
-		if 'gateway' in item:	
-			payload['gateway'] = config[sw][item]
-			r = requests.post(url, data=json.dumps(payload))
-			print(r.status_code)
-			payload = {}
+	if r.status_code is 200:
+		print('SW'+ str(dpid) + '. Added flow.' )
+	else:
+		print('SW'+ str(dpid) + '. None added flow.' )
 
 
+def modify_flow(dpid, priority, src_mac, in_port, output_port):
+        url = controller + '/stats/flowentry/modify'
 
-try:
-	config = configparser.ConfigParser()
-	config.read('config')
-except Error as e:
-	print("Error read config file: " + e )
+        data = {}
+        data['dpid'] = dpid
+        data['priority'] = priority
+        data['match'] = {'eth_src': src_mac, 'in_port': in_port}
+        data['actions'] = [{'type': 'OUTPUT', 'port': output_port}]
 
-cntrllr_url = config['Controller']['url']
+        r = requests.post(url, json.dumps(data))
 
-sw_conf('1')
-sw_conf('2')
-sw_conf('3')
+        if r.status_code is 200:
+                print('SW'+ str(dpid) + '. Modified flow.' )
+        else:
+                print('SW'+ str(dpid) + '. None modified flow.' )
+
+def switch_route(state):
+	if state is backupRoute:
+		modify_flow(1, 65535, h1_mac, 1, 3)
+		modify_flow(3, 65535, h3_mac, 1, 3)
+	
+	if state is primaryRoute:
+		modify_flow(1, 65535, h1_mac, 1, 4)
+                modify_flow(3, 65535, h3_mac, 1, 4)
+
+
+#config switch 1
+add_flow(1, 65535, h1_mac, 1, 4)
+add_flow(1, 65535, h3_mac, 4, 1)
+add_flow(1, 65535, h2_mac, 2, 4)
+add_flow(1, 65535, h4_mac, 4, 2)
+add_flow(1, 65535, h3_mac, 3, 1)
+
+#config switch 3
+add_flow(3, 65535, h3_mac, 1, 4)
+add_flow(3, 65535, h1_mac, 4, 1)
+add_flow(3, 65535, h4_mac, 2, 4)
+add_flow(3, 65535, h2_mac, 4, 2)
+add_flow(3, 65535, h1_mac, 3, 1)
+
+#config switch 2
+add_flow(2, 65535, h1_mac, 1, 2)
+add_flow(2, 65535, h3_mac, 2, 1)
 
 
 totalTxBytes = getPortStatsInfo() 
-i=0
+totalTxBytesBckp = getPortStatsInfo1()
 
 while True:
-	i += 1
 	oldTxBytes = totalTxBytes
 	totalTxBytes = getPortStatsInfo()
+	currentTxBytes = totalTxBytes - oldTxBytes
+
+	oldTxBytesBckp = totalTxBytesBckp
+	totalTxBytesBckp = getPortStatsInfo1()
+	currentTxBytesBckp = totalTxBytesBckp - oldTxBytesBckp
 	
 	if totalTxBytes != oldTxBytes:
-		if (totalTxBytes - oldTxBytes) > (max_speed * upperThreshold) and state is primaryRoute:
+		if (currentTxBytes) > (max_speed * upperThreshold) and state is primaryRoute:
 			state = backupRoute
-			switchRoute(state)
+			switch_route(state)
 		
-		if (totalTxBytes - oldTxBytes) < (max_speed * lowerThreshold) and state is backupRoute:
+		if (currentTxBytes) < (max_speed * lowerThreshold) and state is backupRoute:
 			state = primaryRoute
-                        switchRoute(state)
-
-#	print(getPortStatsInfo())
-	print(i)
+                        switch_route(state)
+	os.system('clear')
+	print("SW    Num Port   Tx_Bytes   Tx_Bytes/s")
+	print("--------------------------------------")
+	print('1     4          ' + str(round(totalTxBytes/B_to_MB, 2)) + 'MB        ' + str(round(currentTxBytes/B_to_MB, 2)) + 'MB/s')
+	print('2     2          ' + str(round(totalTxBytesBckp/B_to_MB, 2)) + 'MB        ' + str(round(currentTxBytesBckp/B_to_MB, 2)) + 'MB/s')
 	time.sleep(1)
